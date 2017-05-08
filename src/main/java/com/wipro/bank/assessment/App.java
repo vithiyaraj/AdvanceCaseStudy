b270130a59cb92cbc0a76660e6ce928ef96a03f6
Bhar Vithi <bhar_vithi@sbsa.com>
2017-05-08 23:07:55 +0200

    initial commit

--- src/main/java/com/wipro/bank/assessment/App.java
+++ src/main/java/com/wipro/bank/assessment/App.java
@@ -1,13 +1,58 @@
 package com.wipro.bank.assessment;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.wipro.bank.entity.Account;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+public class App{
+	
+	static List<Account> accounts;
+	App(){
+		accounts=new ArrayList<Account>();
+		Account a1=new Account(1,"test1",100.00);
+		Account a2=new Account(2,"test2",100.00);
+		Account a3=new Account(3,"test3",200.00);
+		accounts.add(a1);
+		accounts.add(a2);
+		accounts.add(a3);
+		
+	}
+	
+	 
+	
+	public Account getAccountDetailsByID(int id){
+		Account acc=null;
+		for(Account a:accounts){
+			if(a.getAccountID()==id){
+				acc=a;
+			}
+		}
+		return acc;
+		
+		
+	}
+	
+	static List<Account> accounts2=null;
+	public List<Account> getAccountDetailsByBalance(double amount){
+	
+		accounts2=new ArrayList<Account>();
+				
+		for(Account a:accounts){
+			if(a.getBalance()==amount){
+				
+				accounts2.add(a);
+			
+			}
+		}
+		return accounts2;
+		
+	}
+
+	
+   
+} 
--- src/test/java/com/wipro/bank/assessment/AppTest.java
+++ src/test/java/com/wipro/bank/assessment/AppTest.java
@@ -1,38 +1,53 @@
 package com.wipro.bank.assessment;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import java.util.ArrayList;
+import java.util.List;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-}
+import com.wipro.bank.assessment.App;
+import com.wipro.bank.entity.Account;
+
+
+public class AppTest{
+	
+	App app=null;
+	
+	@Before
+	public void  before(){
+				
+		app=new App();
+	}
+	
+	 @After
+	public void after(){
+		 app=null;
+		
+	}
+	 @Test
+	 public void getAccountDetailsByIDTest(){
+		 
+		 assertEquals(App.accounts.get(0),app.getAccountDetailsByID(1));
+		 assertEquals(App.accounts.get(1),app.getAccountDetailsByID(2));
+		 assertNull(app.getAccountDetailsByID(4));
+		 assertNotNull(app.getAccountDetailsByID(2));
+		 
+		 
+		 
+	 }
+	 @Test
+	 public void getAccountDetailsByBalanceTest(){
+		 List<Account> acc=new ArrayList<Account>();
+		acc.add(App.accounts.get(0));
+		acc.add(App.accounts.get(1));
+		 assertEquals(acc,app.getAccountDetailsByBalance(100.00));
+		 assertEquals(acc.size(),app.getAccountDetailsByBalance(100.00).size());
+		 assertNotNull(app.getAccountDetailsByBalance(100.00));
+		 
+	 }
+	
+} 
